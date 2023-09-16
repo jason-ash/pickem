a59dfd7b8c0b0aa8866075a47361daf5d962b509
Jason Ash <jason.ash@symetra.com>
2023-09-15 21:40:00 -0700

    rename migrations file

--- migrations/000_setup.sql
+++ migrations/000_setup.sql
@@ -7,15 +7,15 @@ create table team (
   abbreviation text not null
 ) strict;
 
-create table schedule (
+create table game (
   week int not null,
-  game int not null,
+  game_number int not null,
   game_time text not null,
   away_team text not null,
   home_team text not null,
   away_score int,
   home_score int,
-  primary key (week, game),
+  primary key (week, game_number),
   foreign key (away_team) references team(name),
   foreign key (home_team) references team(name)
 ) strict;
@@ -23,23 +23,23 @@ create table schedule (
 create table pick (
   player_name text not null,
   week int not null,
-  game int not null,
+  game_number int not null,
   winning_team text not null,
   confidence int not null,
   method text not null,
   submission_time_utc text default current_timestamp,
   grace_ind int not null check (grace_ind in (0, 1)),
-  primary key (week, game, player_name, submission_time_utc),
+  primary key (week, game_number, player_name, submission_time_utc),
   foreign key (player_name) references player(name),
   foreign key (winning_team) references team(name)
 ) strict;
 
 create table matchup (
   week int not null,
-  game int not null,
+  game_number int not null,
   player_1 text not null,
   player_2 text not null,
-  primary key (week, game),
+  primary key (week, game_number),
   foreign key (player_1) references player(name),
   foreign key (player_2) references player(name)
 ) strict;
